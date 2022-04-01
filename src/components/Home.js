5e49116e98f6a83ce5a0c3216db0e11f5ef51f27
Paulina Stadnik <paulina.stadnik@rover.com>
2022-04-01 14:26:54 +0200

    iteration 1 done

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom"
+import Beers from "../assets/beers.png"
+import NewBeer from "../assets/new-beer.png"
+import RandomBeer from "../assets/random-beer.png"
+
+export function Home() {
+  return (
+    <div className="homeContainer"> 
+      <div className="homeSectionContainer">
+        <img src={Beers}/>
+        <Link to="/beers" className="title"><h2>All Beers</h2></Link>
+        <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+      </div> 
+
+      <div className="homeSectionContainer">
+        <img src={RandomBeer}/>
+        <Link to="/random-beer" className="title"><h2>Random Beer</h2></Link>
+        <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+      </div> 
+
+      <div className="homeSectionContainer">
+        <img src={NewBeer}/>
+        <Link to="/new-beer" className="title"><h2>New Beer</h2></Link>
+        <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+      </div> 
+
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import { Home } from './components/Home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/beers" element={} />
+        <Route path="/randombeer" element={} />
+        <Route path="/newbeer" element={} /> */}
+        
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
